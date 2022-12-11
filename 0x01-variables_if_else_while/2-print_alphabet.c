311c0df8384306cf955873512afd39882cc470c3
joemainer <mercywkiarie@gmail.com>
2022-12-11 13:46:42 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		int n;
+		char alpha;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					return (0);
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			putchar(alpha);
+		putchar('\n');
+		return (0);
 }
